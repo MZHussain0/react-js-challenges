e1f8c0f5f4795bc3c96dbcec707887c47ac0b7b9
mzhussain0 <mzhussain092@gmail.com>
2023-03-17 21:21:58 +0530

    multidropdown done

---
+++ multiselect-dropdown/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import StateDropdown from "./components/StateDropdown";
+import "./css/stateDropdown.css";
+
+function App() {
+  return (
+    <div>
+      <StateDropdown />
+    </div>
+  );
+}
+
+export default App;
---
+++ multiselect-dropdown/src/components/StateDropdown.tsx
@@ -0,0 +1,68 @@
+﻿import { useEffect, useRef, useState } from "react";
+import { states } from "../data/states";
+
+type Props = {};
+
+const StateDropdown = (props: Props) => {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [selectedStates, setselectedStates] = useState<string[]>([]);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: any) => {
+      if (event.target !== ref.current) {
+        setShowDropdown(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const handleCheckboxChange = (option: string) => {
+    if (selectedStates.includes(option)) {
+      setselectedStates(
+        selectedStates.filter((selectedOption) => selectedOption !== option)
+      );
+    } else {
+      setselectedStates([...selectedStates, option]);
+    }
+  };
+
+  const numberOfSelectedStates = selectedStates.length;
+  return (
+    <div ref={ref} className="state-dropdown">
+      <button
+        onClick={() => {
+          setShowDropdown((prev) => !prev);
+        }}
+        className="state-dropdown">
+        {numberOfSelectedStates > 0
+          ? numberOfSelectedStates + " state selected"
+          : `-- select your state --`}
+      </button>
+      {showDropdown && (
+        <div className="panel">
+          {states.map((state) => (
+            <div
+              className={`panel-items ${
+                selectedStates.includes(state.name) ? "selected" : ""
+              }`}
+              key={state.name}>
+              <input
+                checked={selectedStates.includes(state?.name)}
+                onChange={() => handleCheckboxChange(state?.name)}
+                type="checkbox"
+                id={`${state.name}`}
+              />
+              <label htmlFor={`${state.name}`}>{state.name}</label>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default StateDropdown;
